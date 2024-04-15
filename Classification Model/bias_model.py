7ccab26f91362aff38bb5138a7e3816f870361ef
Diogo Faria <dluis34@gmail.com>
2024-04-15 10:22:29 +0100

    Python now reading from sqlite db

--- Classification Model/bias_model.py
+++ Classification Model/bias_model.py
@@ -1,13 +1,36 @@
 import pandas as pd
 import numpy as np
 import sys
+import sqlite3
 
 from joblib import dump, load
 from sklearn.base import clone
-from sklearn.model_selection import train_test_split
 
 def bias(authors, model_path, data_path):
-    df = pd.read_csv(data_path)
+    conn = sqlite3.connect(data_path)
+    cursor = conn.cursor()
+    
+    cursor.execute("SELECT author, numberLinesOfCodeBef, numberCommentsBef, " +
+                   "numberBlankLinesBef, totalLinesBef, numParametersBef, " +
+                   "numStatementsBef, halsteadLengthBef, halsteadVocabularyBef, " +
+                   "halsteadVolumeBef, halsteadDifficultyBef, halsteadEffortBef, " +
+                   "halsteadLevelBef, halsteadTimeBef, halsteadBugsDeliveredBef, " +
+                   "halsteadMaintainabilityBef, cyclomaticComplexityBef, " +
+                   "cognitiveComplexityBef, lackOfCohesionInMethodBef " +
+                   "FROM metrics")
+    rows = cursor.fetchall()
+    conn.close()
+    
+    df = pd.DataFrame(rows, columns=[
+        'author', 'numberLinesOfCodeBef', 'numberCommentsBef',
+        'numberBlankLinesBef', 'totalLinesBef', 'numParametersBef',
+        'numStatementsBef', 'halsteadLengthBef', 'halsteadVocabularyBef',
+        'halsteadVolumeBef', 'halsteadDifficultyBef', 'halsteadEffortBef',
+        'halsteadLevelBef', 'halsteadTimeBef', 'halsteadBugsDeliveredBef',
+        'halsteadMaintainabilityBef', 'cyclomaticComplexityBef',
+        'cognitiveComplexityBef', 'lackOfCohesionInMethodBef'
+    ])
+    
     df['author'] = df['author'].apply(lambda x: 1 if x in authors else 0)
     
     sample_weights = np.where(df['author'] == 1, 2, 1)
